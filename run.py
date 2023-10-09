5e0ff287ff1c4bcedee1c7912bca6503b7f849f2
deklyn00 <>
2023-10-09 14:48:01 +0000

    Creating a pass type display function for the grades

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import gspread
+import math
 from google.oauth2.service_account import Credentials
 
 # Define constants for credentials and scope
@@ -25,21 +26,21 @@ def calculate_total_grade(student, worksheet):
     total_grade = sum(map(float, grades))
     return total_grade
 
-#Getting the report card of the learner
+# Getting the report card of the learner
 def display_student_report(student, worksheets):
     print(student)
-
+    sum_of_grades = 0
     for sheet_name, worksheet in worksheets.items():
         total_grade = calculate_total_grade(student, worksheet)
         if total_grade is not None:
 
-
-            print(f"{sheet_name.capitalize()}:\t {total_grade}%")
+            sum_of_grades += total_grade
+            type_of_pass_str = type_of_pass(total_grade)
+            print(f"{sheet_name.capitalize()}:\t {total_grade}%\t{type_of_pass_str}")
         else:
-
             print(f"{sheet_name.capitalize()}:\t Grade not found")
+    print(f"Average Grade:\t {round(sum_of_grades/7, 0)}%")
     main()
-
 
 # Validate student name
 def is_valid_student_name(student):
@@ -69,6 +70,17 @@ def main():
     }
 
     display_student_report(student, worksheets)
+
+# Creating a function to determine the level of pass and return the type
+def type_of_pass(grade):
+    if grade < 50.00:
+        return "Fail"
+    elif grade >= 50.00 and grade < 70.00:
+        return "Pass"
+    elif grade >= 70.00 and grade < 80.00:
+        return "Merit"
+    elif grade >= 80.00:
+        return "Distinction"
 
 if __name__ == "__main__":
     main()
